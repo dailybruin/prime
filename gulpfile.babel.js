109ac035dbb0efe8b70ce5066e1343d023f37ff8
Nathan Smith <nathan@nathansmith.io>
2017-08-22 20:02:58 -0700

    Add webpack for javascript

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import nunjucksRender from 'gulp-nunjucks-render';
 import sass from 'gulp-sass';
+import webpack from 'webpack-stream';
 import sourcemaps from 'gulp-sourcemaps';
 import bs from 'browser-sync';
 
@@ -8,13 +9,26 @@ const browserSync = bs.create();
 
 gulp.task('default', () => {});
 
-gulp.task('style', () =>
+gulp.task('styles', () =>
   gulp
     .src('./src/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css'))
+);
+
+gulp.task('scripts', () =>
+  gulp
+    .src('src/js/index.js')
+    .pipe(
+      webpack({
+        output: {
+          filename: 'bundle.js',
+        },
+      })
+    )
+    .pipe(gulp.dest('build/js'))
 );
 
 gulp.task('html', () =>
