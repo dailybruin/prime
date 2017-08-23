bf5effda4db68a930b45b22d6cd8b8f38d8eab9d
Nathan Smith <nathan@nathansmith.io>
2017-08-22 19:36:25 -0700

    Use nunjucks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,28 @@
 import gulp from 'gulp';
+import nunjucksRender from 'gulp-nunjucks-render';
+import bs from 'browser-sync';
 
-gulp.task('default', () => {
-  console.log('hello');
+const browserSync = bs.create();
+
+gulp.task('default', () => {});
+
+gulp.task('html', () =>
+  gulp
+    .src('src/*.html')
+    .pipe(
+      nunjucksRender({
+        path: ['src/partials/'],
+      })
+    )
+    .pipe(gulp.dest('build/'))
+);
+
+gulp.task('browser-sync', () => {
+  gulp.task('browser-sync', () => {
+    browserSync.init({
+      server: {
+        baseDir: './build',
+      },
+    });
+  });
 });
