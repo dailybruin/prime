59132286eaecb919fad47faec5bd168d06b94ff8
Nathan Smith <nathan@nathansmith.io>
2017-08-23 22:27:03 -0700

    Get gulp build running

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,23 +4,29 @@ import nunjucksRender from 'gulp-nunjucks-render';
 // images
 import imagemin from 'gulp-imagemin';
 
+// html
+import htmlmin from 'gulp-htmlmin';
+
 // Styling related packages
 import sass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import minifyCSS from 'gulp-csso';
 
+// javasacript
 import webpack from 'webpack-stream';
+import uglify from 'gulp-uglify';
+
 import sourcemaps from 'gulp-sourcemaps';
 import bs from 'browser-sync';
 import del from 'del';
 
 const browserSync = bs.create();
 
-gulp.task('clean', () => del(['build/']));
+gulp.task('images:dev', () => gulp.src('src/img/*').pipe(gulp.dest('dev/img')));
 
 gulp.task('images:prod', () =>
-  gulp.src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'))
+  gulp.src('src/img/*').pipe(imagemin()).pipe(gulp.dest('prod/img'))
 );
 
 gulp.task('styles:dev', () =>
@@ -30,7 +36,7 @@ gulp.task('styles:dev', () =>
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'))
+    .pipe(gulp.dest('./dev/css'))
     .pipe(browserSync.stream())
 );
 
@@ -40,24 +46,50 @@ gulp.task('styles:prod', () =>
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(minifyCSS())
-    .pipe.pipe(gulp.dest('./build/css'))
+    .pipe(gulp.dest('./prod/css'))
     .pipe(browserSync.stream())
 );
 
-gulp.task('scripts', () =>
+const webpackConfig = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+  },
+};
+
+gulp.task('scripts:dev', () =>
   gulp
     .src('src/js/index.js')
-    .pipe(
-      webpack({
-        output: {
-          filename: 'bundle.js',
-        },
-      })
-    )
-    .pipe(gulp.dest('build/js'))
+    .pipe(sourcemaps.init())
+    .pipe(webpack(webpackConfig))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dev/js'))
 );
 
-gulp.task('html', () =>
+gulp.task('scripts:prod', () =>
+  gulp
+    .src('src/js/index.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(
+      uglify().on('error', e => {
+        console.log(e);
+      })
+    )
+    .pipe(gulp.dest('prod/js'))
+);
+
+gulp.task('html:dev', () =>
   gulp
     .src('src/*.{njk,html}')
     .pipe(
@@ -65,19 +97,44 @@ gulp.task('html', () =>
         path: ['src/partials/'],
       })
     )
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest('dev/'))
 );
 
-gulp.task('browser-sync', ['html', 'styles', 'scripts'], () => {
-  browserSync.init({
-    server: {
-      baseDir: './build',
-    },
-  });
+gulp.task('html:prod', () =>
+  gulp
+    .src('src/*.{njk,html}')
+    .pipe(
+      nunjucksRender({
+        path: ['src/partials/'],
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('prod/'))
+);
 
-  gulp.watch('./src/scss/**/*.scss', ['styles']);
-  gulp.watch('./src/js/**/*.js', ['scripts']);
-  // gulp.watch('src/index.njk').on('change', browserSync.reload);
-});
+gulp.task(
+  'development',
+  ['html:dev', 'styles:dev', 'scripts:dev', 'images:dev'],
+  () => {
+    browserSync.init({
+      server: {
+        baseDir: './build',
+      },
+    });
 
-gulp.task('default', ['browser-sync']);
+    gulp.watch('./src/scss/**/*.scss', ['styles:dev']);
+    gulp.watch('./src/js/**/*.js', ['scripts:dev']);
+    gulp.watch('src/*.{njk,html}').on('change', browserSync.reload);
+  }
+);
+
+gulp.task('production', [
+  'html:prod',
+  'styles:prod',
+  'scripts:prod',
+  'images:prod',
+]);
+
+gulp.task('clean', () => del(['dev/', 'prod/']));
+gulp.task('default', ['development']);
+gulp.task('build', ['production']);
