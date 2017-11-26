ac905b57c5f1aa4245828562dc4329f8abbd3436
Hongyi Zhang <hongee.tc@gmail.com>
2017-11-26 00:31:50 -0800

    responsive topbars, css for phonetabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 export default function main() {
-  console.log('Hello!');
+  const $window = $(window);
+  let ratio = $window.height() / $window.width();
+
+  $(document).ready(() => {
+    const $topbar = $('#top-bar');
+    const $topBarLogoElem = $('#top-bar-logo');
+    const $leftBarLogoElem = $('#left-bar-logo');
+
+    $(window).scroll(() => {
+      if ($(window).scrollTop() >= 800) {
+        if (!$topBarLogoElem.hasClass('logo-hidden')) {
+          $topBarLogoElem.addClass('logo-hidden');
+        }
+        if ($leftBarLogoElem.hasClass('logo-hidden')) {
+          $leftBarLogoElem.removeClass('logo-hidden');
+        }
+        if ($topbar.hasClass('hidden')) {
+          $topbar.removeClass('hidden');
+        }
+      } else {
+        if ($topBarLogoElem.hasClass('logo-hidden')) {
+          $topBarLogoElem.removeClass('logo-hidden');
+        }
+        if (!$leftBarLogoElem.hasClass('logo-hidden')) {
+          $leftBarLogoElem.addClass('logo-hidden');
+        }
+        if (!$topbar.hasClass('hidden')) {
+          $topbar.addClass('hidden');
+        }
+      }
+    });
+  });
 }
