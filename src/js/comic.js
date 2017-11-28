51e3f87ad1b4b987b18d222127d45300d2c5f67c
insomnolent <jbcccsun@gmail.com>
2017-11-27 23:11:07 -0800

    re-writing some js files

--- src/js/comic.js
+++ src/js/comic.js
@@ -1,44 +1,72 @@
-$('.my-gallery').each(function() {
-  var $pic = $(this),
-    getItems = function() {
-      var items = [];
-      $pic.find('a').each(function() {
-        var $href = $(this).attr('href'),
-          $size = $(this)
-            .data('size')
-            .split('x'),
-          $width = $size[0],
-          $height = $size[1];
+$('.my-gallery').each(() => {
+  let $pic = $(this);
+  const $pswp = $('.pswp')[0];
+  let items = [];
+  $pic.find('a').each(() => {
+    let $href = $(this).attr('href'),
+      $size = $(this)
+        .data('size')
+        .split('x'),
+      $width = $size[0],
+      $height = $size[1];
 
-        var item = {
-          src: $href,
-          w: $width,
-          h: $height,
-        };
+    items.push({
+      src: $href,
+      w: $width,
+      h: $height,
+    });
+  });
 
-        items.push(item);
-      });
-      return items;
-    };
-
-  var items = getItems();
+  $pic.find('a').each(() => {
+    $(this).onClick(e => {
+      e.preventDefault();
+      console.log('sdfsdf');
+    });
+  });
 });
 
-var $pswp = $('.pswp')[0];
-$pic.on('click', 'figure', function(event) {
-  event.preventDefault();
+// $('.my-gallery').each(function() {
+//   var $pic = $(this);
+//   var getItems = function() {
+//       var items = [];
+//       $pic.find('a').each(function() {
+//         var $href = $(this).attr('href'),
+//           $size = $(this)
+//             .data('size')
+//             .split('x'),
+//           $width = $size[0],
+//           $height = $size[1];
 
-  var $index = $(this).index();
-  var options = {
-    index: $index,
-    bgOpacity: 0.7,
-    showHideOpacity: true,
-  };
+//         var item = {
+//           src: $href,
+//           w: $width,
+//           h: $height,
+//         };
 
-  // Initialize PhotoSwipe
-  var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-  lightBox.init();
-});
+//         items.push(item);
+//       });
+//       return items;
+//     };
+
+//   var items = getItems();
+//   var $pswp = $('.pswp')[0];
+
+// $pic.on('click', 'figure', function(event) {
+//   event.preventDefault();
+
+//   var $index = $(this).index();
+//   var options = {
+//     index: $index,
+//     bgOpacity: 0.7,
+//     showHideOpacity: true,
+//   };
+
+//   // Initialize PhotoSwipe
+//   var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+//   lightBox.init();
+// });
+
+// });
 
 // var initPhotoSwipeFromDOM = function(gallerySelector) {
 
