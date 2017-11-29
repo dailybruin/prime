86f641449e8274a095dfb4e4e01c41a8ff699e1a
Hongyi Zhang <hongee.tc@gmail.com>
2017-11-29 01:55:00 -0800

    index

--- contentparser.js
+++ contentparser.js
@@ -5,13 +5,17 @@ export default function parseContent() {
   return new Promise((resolve, reject) => {
     glob('content/**/*.md', (err, res) => {
       let data = {};
+      let all_stories = [];
       res.map(file => {
         let slug = file.split('/')[2];
+        all_stories.push(slug);
         data[slug] = matter.read(file).data;
+        data[slug].path = '/' + file.split('/')[1] + '/' + slug + '/';
       });
 
       let meta = require('./content/meta.json');
       meta['data'] = data;
+      meta['allstories'] = all_stories;
 
       console.log(meta);
 
