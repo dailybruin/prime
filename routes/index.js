b23c83f4c47c6bf9e26319d071bb8ee5ef5f76ad
Hongyi Zhang <hongee.tc@gmail.com>
2018-03-05 00:36:09 -0800

    working links

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,7 @@ exports = module.exports = function (app) {
 	app.get('/', routes.views.index);
 	app.get('/:issue/:article', routes.views.article);
 	app.get('/past_issues', routes.views.past_issues);
+	app.get('/about-prime', routes.views.about_prime);
 	app.get('/:section', routes.views.section);
 	// app.get('/:category?', routes.views.section);
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
