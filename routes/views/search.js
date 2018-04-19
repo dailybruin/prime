8f4cf7c392fe607214629905c3d4d0b8b6ec0edf
Jordan Yen <jordanyen726@gmail.com>
2018-04-18 18:55:44 -0700

    search page creation

---
+++ routes/views/search.js
@@ -0,0 +1,9 @@
+var keystone = require("keystone");
+
+exports = module.exports = function(req, res) {
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// Render the view.
+	view.render("search");
+};
