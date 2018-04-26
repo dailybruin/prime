9d85dfee5b830b60338c542b84934194109f1bef
Jordan Yen <jordanyen726@gmail.com>
2018-04-25 17:00:06 -0700

    frontend connected to backend

--- routes/views/search.js
+++ routes/views/search.js
@@ -4,6 +4,8 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
+	console.log(req.query.query);
+
 	// Render the view.
 	view.render("search");
 };
