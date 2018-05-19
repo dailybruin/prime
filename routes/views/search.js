7acebd13fce59e5fa06fa21fbf25f0b90d82a7bf
Jordan Yen <jordanyen726@gmail.com>
2018-05-19 15:43:41 -0700

    setting up backend

--- routes/views/search.js
+++ routes/views/search.js
@@ -6,6 +6,51 @@ exports = module.exports = function(req, res) {
 
 	console.log(req.query.query);
 
+	//insert the search api here and process req.query.query
+	var searchItem = req.query.query;
+
+	locals.section = "search_articles";
+	// locals.filters = {
+	// 	articleSlug: //insert slug from search_api here
+	// };
+	// locals.data = {
+	// 	searchArticle = {},
+	// 	searchArticles: []
+	// };
+
+	// Load the current article.
+	view.on("init", async function(next) {
+		// Load global config.
+		locals.data.config = await keystone.list("Configuration").model.findOne();
+
+		keystone
+			.list("Article")
+			.model.findOne({
+				state: "published",
+				slug: locals.filters.articleSlug
+			})
+			.populate("article")
+			.exec((err, article) => {
+				if (!article) res.status(404).send("Article not found.");
+				locals.data.article = searchArticle;
+				next(err);
+			});
+		// });
+	});
+
+	// Load other articles.
+	view.on("init", function(next) {
+		var q = keystone
+			.list("Article")
+			.model.find()
+			.where("state", "published");
+
+		q.exec(function(err, results) {
+			if (!err) locals.data.searchArticles = results;
+			next(err);
+		});
+	});
+
 	// Render the view.
 	view.render("search");
 };
